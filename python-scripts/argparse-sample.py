8b1a628fc04544f87d5aa60c6a54c812e41cd43c
Ali Abdukarim <aligorithm@icloud.com>
2018-09-03 18:17:32 -0700

    reverse file

--- python-scripts/argparse-sample.py
+++ python-scripts/argparse-sample.py
@@ -14,9 +14,22 @@ import argparse
 """
 
 # build the parser 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Read a file in reverse')
 parser.add_argument('filename', help='the file to read')
+parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+
 
 # parse the arguments 
 args = parser.parse_args()
+print(args)
 # read the file reverse the contents and print 
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+
+    for line in lines:
+        print(line.strip()[::-1])
